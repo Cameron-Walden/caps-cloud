ab4177a4ec921e52decffdb1a88429071e3a23ef
Cameron <cameronkwalden@gmail.com>
2021-11-11 18:10:23 -0800

    Sends out orders every 7000ms

--- vendor2/vendor2.js
+++ vendor2/vendor2.js
@@ -6,18 +6,28 @@ const AWS = require('aws-sdk');
 AWS.config.update({ region: 'us-west-2' });
 const sns = new AWS.SNS();
 
+const faker = require('faker');
+
 //grab this from aws sns topic arn
 const topic = 'arn:aws:sns:us-west-2:801625029834:pickup';
 
+const order = {
+  orderId: faker.datatype.uuid(),
+  customer: faker.name.findName(),
+  vendorId: 777,
+}
+
 const payload = {  
-  Message: 'item has been picked up',
+  Message: JSON.stringify(order),
   TopicArn: topic,
 };
 
-sns.publish(payload).promise()
+setInterval(() => {
+  sns.publish(payload).promise()
   .then(data => {
     console.log(data);
   })
   .catch(error => {
     console.log(error);
   });
+}, 7000);
