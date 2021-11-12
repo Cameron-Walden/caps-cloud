dd58c19bf8573d166534d8f57a36a686dbfed05c
Cameron <cameronkwalden@gmail.com>
2021-11-11 20:52:14 -0800

    requires producer

--- driver/driver.js
+++ driver/driver.js
@@ -3,13 +3,16 @@
 
 const { Consumer } = require('sqs-consumer');
 
+//needed for fifo queue
+const { Producer } = require('sqs-producer');
+
 //grab this from sqs url
 const queueUrl = 'https://sqs.us-west-2.amazonaws.com/801625029834/packages';
 
 const consumer = Consumer.create({
   queueUrl: queueUrl,
-  handleMessage: (message) => {
-    console.log(message);
+  handleMessage: async (message) => {
+    console.log(message, '<--------THIS IS MESSAGE');
   }
 });
 
